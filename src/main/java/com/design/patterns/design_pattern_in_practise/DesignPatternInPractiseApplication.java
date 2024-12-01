54f623426cd59bca6a0ccb14ec221f376100ec34
utkarsh-mbp <tech-config-us@utkarsh-mbp.local>
2024-12-01 14:57:51 +0530

    initial commit

---
+++ src/main/java/com/design/patterns/design_pattern_in_practise/DesignPatternInPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.design.patterns.design_pattern_in_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternInPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternInPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/design/patterns/design_pattern_in_practise/DesignPatternInPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.patterns.design_pattern_in_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternInPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
